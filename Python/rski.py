45bb9add4d5d6e8dd859688dc22199490d8b50ce
rski <rski@intracom-telecom.com>
2015-10-14 09:56:44 +0300

    Fix documentation

--- Python/rski.py
+++ Python/rski.py
@@ -2,7 +2,7 @@
 """I was told lambdas are cool"""
 listOfNumbersToBeFizzBuzzed = [x for x in range(1, 16)]
 
-#special thanks to Greg for telling be about the == operator
+#special thanks to Greg for telling me about the == operator
 fizzbuzz = lambda x: "fizzbuzz" if x % 3 == 0 and x % 5 == 0 else x
 fizz = lambda x: "fizz" if isinstance(x, int) and  x % 3 == 0 else x
 buzz = lambda x: "buzz" if isinstance(x, int)  and x % 5 == 0 else x
